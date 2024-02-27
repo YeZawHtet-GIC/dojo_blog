949772fc6e24fc39321e28386fc29a76a1cb99e1
YeZawHtet-GIC <yezawhtet@gicjp.com>
2024-02-27 08:45:23 +0630

    .

--- src/NotFound.js
+++ src/NotFound.js
@@ -3,10 +3,10 @@ import React from 'react'
 
 function NotFound() {
   return (
-    <div className="not-found">
+    <div className="col-md-6 offset-3 text-center mt-5 bg-dark p-5 rounded text-warning">
       <h2>Sorry</h2>
       <p>That page cannot be found</p>
-      <Link to="/">Back to the homepage...</Link>
+      <Link className="btn btn-outline-primary" to="/">Back to the homepage...</Link>
     </div>
   );
 }
